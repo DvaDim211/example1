16b552e0f2417bee82aab050b7166cc600c1202d
DvaDim211 <dimaberezovskyi27@gmail.com>
2025-05-13 02:36:00 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DummyUsers } from './shared/dummy-users';
+import {TasksComponent} from './tasks/tasks.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'example1';
+  users = DummyUsers;
+  selectedUser?:string  = '';
+  onSelectUser(id: string) {
+    this.selectedUser = id;
+    console.log(id);
+    console.log(this.selectedUser);
+  }
+
 }
